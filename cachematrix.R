537a960cacb3dbd86312eb0951961e80068ed540
bitterpal <bitterpal@users.noreply.github.com>
2014-12-22 02:21:14 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix<-function(x=matrix()){   #function named according to instructions
+  inverse<-NULL #for storing inverse when computed as cache
+  setMatrix<-function(y){ #y:is a numeric matrix,to be saved into the variable 'x'
+    inverse<<-NULL
+    x<<-y
+  }
+  getMatrix<-function() x #prints or returns the saved matrix
+  setInverse<-function(ix){ #saves the inverse 'ix' in cache variable 'inverse'
+    inverse<<-ix
+  }
+  getInverse<-function() inverse #returns the inverse
+  list(setM=setMatrix,getM=getMatrix,getIn=getInverse,setIn=setInverse)#list of function, standard way of returning multiple functions from a closure
+}
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+cacheSolve<-function(matrixList,...){ #named according to instructions, takes previous function's return as argument
+  inverse<-matrixList$getIn()#gets the saved inverse, if not computed yet, NULL is returned
+  if(!is.null(inverse)){
+    print("Inverse already in cache.Printing...")
+    return(inverse)
+  }
+    matrix<-matrixList$getM()#get the matrix to compute the inverse
+    inverse<-solve(matrix,...)
+    matrixList$setIn(inverse)#saves the computed inverse to cache
+    return(inverse)
 }
 
 
-## Write a short comment describing this function
+#I have added a test case,just to make things easier.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+temp<-list(12,23,34,45,65,65,45,23,43)
+testmatrix<-matrix(unlist(temp),3,3)#unlist used because solve only works on numeric matrices 
+m<-makeCacheMatrix(testmatrix)
+cacheSolve(m)#will compute inverse
+cacheSolve(m)#will simply return the already present inverse from the cache
